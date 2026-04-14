6ae9cdd2d2b24bb70ecaa775b7a43d58a4fd81b4
fallyyd <www.wilsonchristian99@gmail.com>
2026-04-14 13:16:32 +0700

    feat: implement student index page b

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,15 +1,23 @@
 <?php
 namespace App\Controllers;
 require_once '../app/core/Controller.php';
+require_once'../app/models/Student.php';
 
 use App\core\Controller;
+use App\models\Student;
 
 class StudentController extends Controller
 {
 
     public function index()
     {
-        $this->view('students.index');
+        $studentModel = new Student();
+        $students = $studentModel->getStudents();
+
+
+        $this->view('students.index', [
+            'students' => $students
+        ]);
     }
 
     public function create()
--- app/views/students/index.php
+++ app/views/students/index.php
@@ -21,20 +21,32 @@
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td class="px-4 py-2 text-left">1</td>
-                    <td class="px-4 py-2 text-left">Andi</td>
-                    <td class="px-4 py-2 text-left">1234</td>
-                    <td class="px-4 py-2 text-left">11 TKJ 2</td>
-                    <td class="px-4 py-2 text-left">082253331895</td>
-                    <td class="px-4 py-2 text-left">
-                        <div class="flex justify-center items-center gap-4">
-                            <a href="/students/1" class="text-green-500">Detail</a>
-                            <a href="/students/1/edit" class="text-yellow-500">Edit</a>
-                            <a href="" class="text-red-500">Hapus</a>
-                        </div>
-                    </td>
-                </tr>
+                <?php foreach ($students as $index => $student): ?>
+                    <tr>
+                        <td class="px-4 py-2 text-left">
+                            <?= $index + 1?>
+                        </td>
+                        <td class="px-4 py-2 text-left">
+                            <?= $student['name'] ?>
+                        </td>
+                        <td class="px-4 py-2 text-left">
+                            <?= $student['nis'] ?>
+                        </td>
+                        <td class="px-4 py-2 text-left">
+                              <?= $student['class'] ?>
+                        </td>
+                        <td class="px-4 py-2 text-left">
+                              <?= $student['phone_number'] ?>
+                        </td>
+                        <td class="px-4 py-2 text-left">
+                            <div class="flex justify-center items-center gap-4">
+                                <a href="/students/<?= $student['id'] ?>" class="text-green-500">Detail</a>
+                                <a href="/students/<?= $student['id']?>/edit" class="text-yellow-500">Edit</a>
+                                <a href="" class="text-red-500">Hapus</a>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
             </tbody>
         </table>
     </div>
