7ab2418ba051132b26d64ef29d69acbc1cf48f52
fallyyd <www.wilsonchristian99@gmail.com>
2026-04-16 13:47:06 +0700

    feat: setup method spoofing

--- app/core/Router.php
+++ app/core/Router.php
@@ -8,40 +8,43 @@ class Router
 {
     private array $routes = [];
 
-    public function add (string $method, string $uri, string $controller, string $function)
+    public function add(string $method, string $uri, string $controller, string $function)
     {
         $this->routes[] = [
             'method' => $method,
             'uri' => $uri,
             'controller' => $controller,
             'function' => $function,
-        ]; 
+        ];
     }
     public function run()
     {
 
         $method = $_SERVER['REQUEST_METHOD'];
+        if ($method === 'POST' && isset($_POST['_method'])) {
+            $method = $_POST['method'];
+        }
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        foreach($this->routes as $route) {
+        foreach ($this->routes as $route) {
             $pattern = str_replace(
                 '{id}',
                 '([0-9])',
-                $route ['uri']
+                $route['uri']
             );
 
             $pattern = '#^' . $pattern . '$#';
 
-            if (preg_match($pattern, $uri, $matches)) {
+            if ($method === $route['method'] && preg_match($pattern, $uri, $matches)) {
                 require_once '../app/controllers/' . $route['controller'] . '.php';
                 array_shift($matches);
                 $controllerClass = 'App\\Controllers\\' . $route['controller'];
                 $controller = new $controllerClass();
-                
+
                 $function = $route['function'];
                 call_user_func_array([$controller, $function], $matches);
 
-                return; 
+                return;
             }
         }
 
